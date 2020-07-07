42f72c5f5a9bc37dc47b21d33449d1705e25b3ad
dowap2 <dowapdowari@gmail.com>
2020-07-08 00:58:51 +0900

    add content infinityScroll

--- src/ContentHome.jsx
+++ src/ContentHome.jsx
@@ -23,58 +23,42 @@ import { getByText } from '@testing-library/dom';
 export function ContentHome() {
   const [modal , setModal] = useState("none")
   const [addPost , setAddPost] = useState([])
-  const [userText , setUserText] = useState("")
   const [postID , setPostID] = useState(0)
-  let userName = "DRX_Deft"
 
-  function getFunc(){
-    axios({
-      method: 'get',
-      url: 'https://randomuser.me/api/',
-      responseType: 'json'
-    })
-    .then(response => {
-      console.log(response.data)
-      addPostFunc(response.data.results)
-    }).catch(err => {
-      console.log(err)
-    })
-  }
-  function addPostFunc(data){
-    if(data != undefined){
-      console.log(data[0].picture.large)
-      const post = <Post
-      more={more} profile={data[0].picture.large} userName={data[0].email} userText={userText}
-      img={"https://picsum.photos/200/300?random="+{postID}}
-      heart={heart} fill_heart={fill_heart} comment={comment} send={send} bookmark={bookmark}
-      />
-      const array = addPost.concat(post)
-      setAddPost(array)
-      setPostID(postID+1)
+  function InfinityScrolling(){
+    if((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+      axios({
+        method: 'get',
+        url: 'https://randomuser.me/api/',
+        responseType: 'json'
+      })
+      .then(response => {
+        console.log(response.data)
+        addPostFunc(response.data.results)
+      }).catch(err => {
+        console.log(err)
+      })
     }
-  }
-  function getPicture(){
-    axios({
-      method: 'get',
-      url: "https://source.unsplash.com/random/800x800",
-      responseType: 'link'
-    })
-    .then(response => {
-      console.log(response.data)
-    }).catch(err => {
-      console.log(err)
-    })
-  }
+    function addPostFunc(data){
+      if(data != undefined){
+        console.log(data[0].picture.large)
+        const post = <Post
+        more={more} profile={data[0].picture.large} userName={data[0].email} img={"https://picsum.photos/200/300?random="+{postID}}
+        heart={heart} fill_heart={fill_heart} comment={comment} send={send} bookmark={bookmark}
+        />
+        const array = addPost.concat(post)
+        setAddPost(array)
+        setPostID(postID+1)
+      }
+    }
+  };
   
   return (
-    <div className="content__home">
+    <div className="content__home" onscroll={InfinityScrolling()}>
       <FollowModal open={modal} setOpen={setModal}/>
         <div className="content__main">
           <div className="story">
             <Story profile={profile}/>
-            <input type="button" value="post" id="btnPost" onClick={e=> getFunc()}/>
-            <input type="button" value="getPicture" id="btnPost" onClick={e=> getPicture()}/>
-            <input type="button" value="addPost" id="btnPost" onClick={e=> console.log(addPost)}/>
           </div>
           <div>
             {addPost}
